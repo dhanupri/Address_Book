fb4b201ac7fd0ee841be6c44d53307307b415db8
unknown <dhanupriasokan@gmail.com>
2023-11-24 18:01:12 +0530

    Display

---
+++ Contact.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Contact {
+   
+
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Welcome to Address Book Program in AddressBookMain");
+    }
+}
+
