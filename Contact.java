5caf60cad5561d1e456d70b8fa093f4f9e465cf8
unknown <dhanupriasokan@gmail.com>
2023-11-24 18:06:13 +0530

    useacse3

--- Contact.java
+++ Contact.java
@@ -1,5 +1,3 @@
-import Address_book.Address;
-
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -22,7 +20,44 @@ public class Contact {
         this.phone_number=phone_number;
         this.email=email;
     }
+    public String getfirstName() {
+        return firstName;
+    }
+    public String getlastName(){
+        return lastName;
+    }
 
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public void setPhone_number(String phone_number) {
+        this.phone_number = phone_number;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
     @Override
     public String toString() {
@@ -40,40 +75,19 @@ public class Contact {
 
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
+        AddressBook addressBook=new AddressBook();
+
         System.out.println("Welcome to Address Book Program in AddressBookMain");
-
-
-
-
-
-
 
         System.out.println("Want to add new contact yes/no");
         String res=sc.nextLine();
 
 
         while(res.equals("yes")){
-            System.out.println("FirstName:");
-            String FirstName= sc.nextLine();
-            System.out.println("LastName:");
-            String LastName= sc.nextLine();
-            System.out.println("Address:");
-            String Address= sc.nextLine();
-            System.out.println("City:");
-            String City=sc.nextLine();
-            System.out.println("State:");
-            String State= sc.nextLine();
-            System.out.println("ZIP:");
-            String ZIP=sc.nextLine();
-            System.out.println("Phone_number:");
-            String PhoneNumber= sc.nextLine();
-            System.out.println("Email:");
-            String Email=sc.nextLine();
 
 
 
-
-            Contact contact=new Contact(FirstName,LastName,Address,City,State,ZIP,PhoneNumber,Email );
+            Contact contact=new Contact(sc.nextLine(), sc.nextLine(), sc.nextLine(),sc.nextLine(),sc.nextLine(),sc.nextLine(),sc.nextLine(),sc.nextLine() );
             AddressBook.Contact_Information(contact);
             System.out.println("Want to add new contact yes/no");
             res=sc.nextLine();
@@ -84,13 +98,60 @@ public class Contact {
 
         if(res.equals("no")) {
 
-          
+
             AddressBook.display();
+        }
+
+        System.out.println("Enter the name of contact you want to edit:");
+        String contact_name= sc.nextLine();
+        Contact contact_existing=AddressBook.search_contact(contact_name);
+
+        if(contact_existing!=null){
+            System.out.println("Existing Contact Details:");
+            System.out.println(contact_existing);
+            System.out.println("Enter the new details:");
+
+            System.out.println("FirstName:");
+            contact_existing.setFirstName(sc.nextLine());
+
+
+            System.out.println("LastName:");
+            contact_existing.setLastName(sc.nextLine());
+
+            System.out.println("Address:");
+            contact_existing.setAddress(sc.nextLine());
+            System.out.println("City:");
+            contact_existing.setCity(sc.nextLine());
+            System.out.println("State:");
+            contact_existing.setState(sc.nextLine());
+            System.out.println("ZIP:");
+            contact_existing.setZip(sc.nextLine());
+            System.out.println("Phone_number:");
+            contact_existing.setPhone_number(sc.nextLine());
+            System.out.println("Email:");
+            contact_existing.setEmail(sc.nextLine());
+
+            System.out.println("Updated Contact Details");
+            System.out.println(contact_existing);
+
+            System.out.println("AddressBookMain:");
+            AddressBook.display();
+
+
+
+
+
+
+
+        }
+        else{
+            System.out.println("Contact not found");
         }
 
 
 
     }
+
 
 }
 class AddressBook{
@@ -106,6 +167,16 @@ class AddressBook{
         for(Contact c:contact_Information){
             System.out.println(c);
         }
+    }
+
+    public static Contact search_contact(String name){
+        for(Contact c:contact_Information){
+            if(c.getfirstName().equals(name) || c.getlastName().equals(name)){
+                return c;
+            }
+
+        }
+        return null;
     }
 
 }
